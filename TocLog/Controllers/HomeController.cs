9fd4e195307d5eaba2727ddbd0b5ca3fb4193a5a
Kreufeldt <tomas.lidholm@gmail.com>
2015-06-10 15:30:30 +0200

    config

--- TocLog/Controllers/HomeController.cs
+++ TocLog/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace TocLog.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize(Users = "tomas.lidholm@r2m.se")]
+        [Authorize(Users="tomas.lidholm@r2m.onmicrosoft.com")]
         public ActionResult Index()
         {
             return View();
