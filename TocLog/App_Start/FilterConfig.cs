93f67f562e48484f1898e3b0566c211f8243ef20
Kreufeldt <tomas.lidholm@gmail.com>
2015-06-10 14:08:13 +0200

    initial

---
+++ TocLog/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TocLog
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TocLog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TocLog.MvcApplication" Language="C#" %>
